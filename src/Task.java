a6b64752e81e304170c9c42158385f8d6de2695e
LuckyLoki <luckyloki666@ya.ru>
2024-02-11 19:45:18 +0400

    балуемся с компоратором =)

--- src/Task.java
+++ src/Task.java
@@ -2,7 +2,6 @@ import java.time.LocalDateTime;
 public class Task {
     public String name;
     public LocalDateTime start;
-
     public Task(String name, LocalDateTime start) {
         this.name = name;
         this.start = start;
